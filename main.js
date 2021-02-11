12058f4e7cad797c1debabdc87a53cb40a733144
Uzair Ali <uzairali19b@gmail.com>
2021-02-11 20:42:07 +0500

    final-commit

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 // const a = document.querySelectorAll('a');
 
 // a.scroll(0, -100000);
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            top:200
+        });
+    });
+});
